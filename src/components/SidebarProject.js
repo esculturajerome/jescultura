823f8f30e0f048dfa860dc497bdaf3324197051e
esculturajerome <esculturajerome@gmail.com>
2020-07-14 16:03:51 +0800

    jescultura | Changed logos and Fav

--- src/components/SidebarProject.js
+++ src/components/SidebarProject.js
@@ -1,33 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Spring } from 'react-spring/renderprops';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Spring } from "react-spring/renderprops";
 
-class SidebarProject extends React.Component{
+class SidebarProject extends React.Component {
   render() {
     return (
-      <Spring
-        from={{ opacity: 0 }}
-        to={{ opacity: 1 }}
-        >
-        { props => (
+      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {(props) => (
           <div style={props}>
             <header id="sidebar">
               <div className="sidebar-inner">
                 <Link to="/" className="logo smooth-leavess">
-                  <img className="img-responsive" src="/logo192.png" alt="Jerome Escultura Profile" />
+                  <img className="img-responsive" src="/logo.svg" alt="Logo" />
                 </Link>
-                <p className="intro">A Minimalist tech enthusiast from Manila</p>
+                <p className="intro">
+                  A Minimalist tech enthusiast from Manila
+                </p>
                 <nav>
                   <ul>
                     <Spring
-                    from={{ opacity: 0, marginRight: -70 }}
-                    to={{ opacity: 1, marginRight: 0 }}
+                      from={{ opacity: 0, marginRight: -70 }}
+                      to={{ opacity: 1, marginRight: 0 }}
                     >
-                      {props => (
+                      {(props) => (
                         <div style={props}>
-                          <li><Link to="/"  ><span>Personal Projects</span></Link></li>
-                          <li><Link to="/works" ><span>Work Projects</span></Link></li>
-                          <li><Link to="/about" ><span>About</span></Link></li>
+                          <li>
+                            <Link to="/">
+                              <span>Personal Projects</span>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/works">
+                              <span>Work Projects</span>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/about">
+                              <span>About</span>
+                            </Link>
+                          </li>
                         </div>
                       )}
                     </Spring>
