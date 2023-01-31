2ebf4008fde8efd97ef98611dc458962737c34bb
Jerome Escultura <esculturajerome@gmail.com>
2023-01-31 17:52:16 +0800

    Added voluntary page

---
+++ pages/voluntary.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const voluntary = () => {
+  return (
+    <div className="p-24 flex gap-24">
+      <div className="">
+        <h4 className="mb-12 text-2xl">Agency Youth Council President</h4>
+        <p>
+          Oversees the duties and functions for both Agency Level and Center
+          level Youth Council. Serves as the head of the Core Group in{" "}
+          <a
+            href="https://www.children.org/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Children International
+          </a>
+          .
+        </p>
+        <p>
+          Represents the Youth Council and its programs by serving as member of
+          the Agency’s Board of Trustees (BOT) during my term of office.
+        </p>
+        <p>
+          Actively presides meetings and prepares agenda for the Core Group to
+          be disseminated through the Agency Youth Council Vice President for
+          Monitoring and Evaluation.
+        </p>
+      </div>
+      <div className="">
+        <h4 className="mb-12 text-2xl">Agency Youth Council Vice President</h4>
+        <p>
+          Responsible in working closely with Agency Youth Council and Youth
+          Program Coordinator In the planning and preparation of the needed
+          Council funds to be allotted to project implementation in{" "}
+          <a
+            href="https://www.children.org/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Children International
+          </a>
+          .
+        </p>
+        <p>
+          Keeps all financial records (liquidations) and other documents
+          properly filed for accounting and other auditing purposes to be
+          submitted to specific Field Officer and to the Youth Program
+          Coordinator.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default voluntary;
